04b30526d1d37edfd3a76d6e607cf484f9375f4c
Savianok Marta <magnolia.sav@gmail.com>
2024-06-20 18:05:17 +0300

    end

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,28 @@ closeMenuBtn?.addEventListener('click' , ()=> {
 overlay?.addEventListener('click' , ()=> {
     hideMenuModal()
 })
+
+const phoneSelect = document.querySelector('.phone-select .header-phones');
+const phoneSelectWrap = document.querySelector('.phone-select');
+
+console.log(phoneSelectWrap)
+
+if (phoneSelect) {
+    phoneSelect.addEventListener('click' , (e)=> {
+        e.stopPropagation()
+        let currentSelectWrap = e.target.closest('.phone-select');
+        let currentSelectBody = currentSelectWrap.querySelector('.phone-select-wrapper');
+        currentSelectBody.classList.toggle('active');
+        phoneSelect.classList.toggle('active');
+        phoneSelectWrap.classList.toggle('active');
+    })
+    document.addEventListener('click', (e)=> {
+        let lists = document.querySelectorAll('.phone-select-wrapper.active')
+        if (!e.target.closest('.phone-select') && lists){
+            lists.forEach(el=> {
+                el.classList.remove('active');
+                phoneSelectWrap.classList.remove('active');
+            })
+        }
+    })
+}
